3e414cb88a5917d1ad448d6466f5b0570076a5e7
Anuj Gupta <guptaanuj0811@gmail.com>
2020-07-23 16:24:28 +0530

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+let isDarkMode = false;
+(function ($) {
+
+  "use strict";
+
+  // COLOR MODE
+  $('.color-mode').click(function () {
+    $('.color-mode-icon').toggleClass('active')
+    isDarkMode = !isDarkMode;
+    $('body').toggleClass('dark-mode');
+    isDarkMode && $("#users-icon").attr("src", "images/icons/user-white.svg") && $('.climate').attr("src", "images/icons/sun.svg");
+    !isDarkMode && $("#users-icon").attr("src", "images/icons/user.svg") && $('.climate').attr("src", "images/icons/moon.svg");
+  });
+
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true
+  });
+
+  // SMOOTHSCROLL
+  $(function () {
+    $('.nav-link, .custom-btn-link').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+
+
+})(jQuery);
